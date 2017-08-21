0b6b9043d3a9a98b9935d28dac3b8cb50170cb55
Nick Smart <npsmart@umich.edu>
2017-08-21 00:57:45 -0700

    Added header photo

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../styles/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header_photo">
+          
+        </div>
+        Hello world! This is my website
+      </div>
+    );
+  }
+}
+
+export default Home;
