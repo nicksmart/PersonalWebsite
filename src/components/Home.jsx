96ba9c0445ee0bf9e645ee2ec773e4e2cc0fffe4
Nick Smart <npsmart@umich.edu>
2017-08-22 01:06:05 -0700

    Updated my info stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,16 @@ class Home extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header_photo">
-          
+        <div className="App-header_photo" />
+        <div className="App-header">
+          <h1 className="App-header-myname">Nick Smart</h1>
+          <p className="App-header-myinfo">
+            Software Engineer<br>
+            </br>University of Michigan<br>
+            </br>Computer Science Engineer in the College of Engineering<br>
+            </br>Business Minor in the Stephen M. Ross School of Business<br></br>
+          </p>
         </div>
-        Hello world! This is my website
       </div>
     );
   }
