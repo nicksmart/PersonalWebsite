9fe35ea0a6beaa8aee77f757b54e3e83639083fa
Nick Smart <npsmart@umich.edu>
2017-08-28 11:17:00 -0700

    Updated font sizes

--- src/components/Recruit.jsx
+++ src/components/Recruit.jsx
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
-import ReactPDF from 'react-pdf';
 import '../styles/Recruit.css';
 import '../styles/Home.css';
-import ResumeIcon from '../assets/images/resume_icon.png';
-import Resume from '../assets/documents/Resume.pdf';
+// import ResumeIcon from '../assets/images/resume_icon.png';
+import ResumeIcon2 from '../assets/images/resume_icon2.png';
+import Resume from '../assets/documents/Web_Resume.pdf';
 
 export default class Recruit extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showResume: false
-    };
-
-    // this.toggleShowResume = this.toggleShowResume.bind(this);
-  }
-
-  // toggleShowResume() {
-  //   this.setState({
-  //     showResume: !this.state.showResume
-  //   });
-  // }
   
   render() {
-    // const resume = (this.state.showResume) ? <ReactPDF file={Resume} /> : '';
-    
     return(
       <div className="Recruit">
         <h1 className="Content-header Content-header-centered">Recruiting</h1>
         <div className="Recruit-resumediv">
-          <button className="Recruit-resume" target="_blank">
-            <img className="Recruit-resume" src={ResumeIcon} alt="Resume" />
-          </button>
+          <a href={Resume} className="Recruit-resume" target="_blank">
+            <img className="Recruit-resume" src={ResumeIcon2} alt="Resume" />
+          </a>
+        </div>
+        <div className="Recruit-content">
+          <p>
+            I'm currently looking for software engineering internships for the summer of 2018.
+            I would like to have an internship doing backend development work this upcoming summer.
+            I'm open to any location, but I have a strong preference for California.<br /><br />
+            I'm passionate about all things technology and I want to constantly be pushing myself to become a better programmer.
+            I'm also particularly interested in working with technology to help improve people's financial success and wellbeing.<br /><br />
+            A link to my Github can be found  
+            <a href="https://github.com/nicksmart" target="_blank"> here. </a>
+            I have my personal projects posted publicly on there and my school projects posted privately due to my school's Honor Code.<br /><br />
+            If you'd like to reach out to my personally regarding recruiting matters, please send me an email
+            <a href="mailto:npsmart@umich.edu" target="_blank"> here.</a>
+          </p>
         </div>
       </div>
     );
